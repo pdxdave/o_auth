fb399744811d8b174a6c619ad088cb40801445bb
pdxdave <webdevdave@hotmail.com>
2020-04-23 15:52:53 -0700

    made callback page and handleAuth and setSession funcs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Route} from 'react-router-dom'
 import Home from './Home'
 import Profile from './Profile'
 import Nav from './Nav'
+import Callback from './Callback'
 import Auth from './Auth/Auth';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
       <div className="App">
         <Nav />
         <Route exact path="/" render={props => <Home auth={this.auth} {...props}/> }/>
+        <Route exact path="/callback" render={props => <Callback auth={this.auth} {...props}/> }/>
         <Route path="/profile" component={Profile}/>
       </div>
     );
